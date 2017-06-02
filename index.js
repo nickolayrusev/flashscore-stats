7d7f6b90bd1fed40eb0adae0adc8b81d4b374194
Nickolay Rusev <nickolay.rusev@gmail.com>
2017-06-02 16:14:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+import fetch from 'node-fetch'
+import $ from 'cheerio'
+import fs from 'fs'
+import {inspect} from 'util'
+import _ from 'lodash'
+
+/**
+ *
+ * @returns {Promise.<*|Array>}
+ */
+const fetchGameIds = async () => {
+    let result = await fetch('http://www.flashscore.mobi/').then(r => r.text());
+    let ids = $('div[id=score-data] > a', result).map((i, e) => {
+            return $(e).attr('href')
+        }
+    ).get();
+    return ids.map(e => (e.substring(7, 15)))
+};
+
+/**
+ *
+ * @param ids
+ * @param all
+ * @returns {Array}
+ */
+const fetchFeed = (ids, all = false) => {
+    if (!all)
+        ids = ids.slice(0, 5)
+    return ids.map(id => {
+        const FEED = `http://d.flashscore.com/x/feed/d_hh_${id}_en_1`;
+        return fetch(FEED, {headers: {'X-Fsign': 'SW9D1eZo'}}).then(r => r.text())
+    })
+};
+
+const extractHeadToHead = (html) => {
+    return $('#tab-h2h-overall .h2h_mutual', html).map((i, e) => {
+        let title = $('table thead > tr', e).text().substring('Head-to-head matches: '.length);
+
+        let games = $('table tbody > tr', e).map((ri, r) => {
+            let cols = $('td', r);
+            if (cols.length > 1)
+                return {
+                    date: $(cols[0]).text(),
+                    league: $(cols[1]).text(),
+                    home: $(cols[2]).text(),
+                    away: $(cols[3]).text(),
+                    score: $(cols[4]).text()
+                };
+        }).get();
+        return {title, games}
+    }).get()[0]
+};
+
+const splitScore = (score) => {
+    if (score.includes('(')) {
+        return score.substring(0, score.indexOf('(')).split(':')
+    }
+    return score.split(':')
+};
+
+let bothTeamsScored = (game) => {
+    const score = splitScore(game.score);
+    let home = score[0].trim(),
+        away = score[1].trim();
+
+    return home !== '0' && away !== '0'
+};
+
+let isOver = (game) => {
+    return totalGoals(game) > 2.5
+};
+
+let totalGoals = (game) => {
+    const score = splitScore(game.score);
+    let home = Number(score[0].trim()),
+        away = Number(score[1].trim());
+    return home + away
+};
+
+const computeStats = (game, size = 5) => {
+    let games = game.games.slice(0,size).map((g) => {
+                return {
+                    ...g,
+                    isOver: isOver(g),
+                    isUnder: !isOver(g),
+                    bothTeamsScored: bothTeamsScored(g),
+                    totalGoals: totalGoals(g)
+                }});
+
+    return {
+        ...game,
+        games:games,
+        overPercentage: games.length === 0 ? '0%' : Math.round(games.filter(g => g.isOver).length / games.length * 100) + '%',
+        underPercentage: games.length === 0 ? '0%' : Math.round(games.filter(g => g.isUnder).length / games.length * 100) + '%',
+        bothTeamsToScorePercentage :games.length === 0 ? '0%' : Math.round(games.filter(g => g.bothTeamsScored).length / games.length * 100) + '%',
+        averageGoals: games.length === 0 ? '0%' : Math.round(_.sum(games.map(t => t.totalGoals)) / games.length),
+        size: games.length
+    }
+};
+
+(async () => {
+    let ids = await fetchGameIds();
+    let result = await Promise.all(fetchFeed(ids, true));
+    console.log('result is ', result.length);
+
+    const obj = result.reduce((i, n) => {
+        let stats = computeStats(extractHeadToHead(n))
+        return [...i, stats]
+    }, []);
+
+    fs.writeFile('result' + '.json', JSON.stringify(obj), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was written!");
+    });
+
+})();
